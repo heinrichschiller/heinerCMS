db004c1a612b13d868005ec515d18e4ac19a730c
Heinrich Schiller <heinrich@heinrich-schiller.de>
2017-04-21 15:25:47 +0200

    new

---
+++ templates/login.php
@@ -0,0 +1,12 @@
+<h4>Login</h4>
+<form action="login.php" method="post">
+	<div class="form-group">
+		<label for="email">Email:</label>
+		<input type="email" name="email" class="form-control" id="email" maxlength="64">
+	</div>
+	<div class="form-group">
+		<label for="passwd">Passwort:</label>
+		<input type="password" name="password" class="form-control" id="passwd">
+	</div>
+	<button type="submit" class="btn btn-success">Login</button>
+</form>
