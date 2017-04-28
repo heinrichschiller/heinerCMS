427668e430508dc4fd5cb2b9fa7c89e6e8a89e1e
heinrich <heinrich@heinrich@heinrich-schiller.de>
2017-04-28 15:57:45 +0200

    update

--- inc/base.inc.php
+++ inc/base.inc.php
@@ -11,8 +11,6 @@ $base['content'] = '';
 $base['template'] = 'templates/main.htm';
 
 $base['adm_title'] = 'heinerCMS - Administration';
-//$base['adm_shortnav'] = '<a href="$PHP_SELF?uri=news">News</a> &middot; <a href="$PHP_SELF?uri=downloads">Downloads</a> &middot; <a href="logout.php">Logout</a>';
-// $base['adm_navigation'] = '<a href="$PHP_SELF?uri=news">News</a><br><a href="$PHP_SELF?uri=downloads">Downloads</a><br><a href="$PHP_SELF?uri=links">Links</a><br><a href="$PHP_SELF?uri=articles">Artikel</a><br><br><a href="logout.php" class="red">Logout</a>';
 $base['adm_navigation'] = '<div class="list-group">
   <a href="$PHP_SELF?uri=news" class="list-group-item">News<span class="badge">0</span></a>
   <a href="$PHP_SELF?uri=downloads" class="list-group-item">Downloads<span class="badge">0</span></a>
@@ -22,6 +20,7 @@ $base['adm_navigation'] = '<div class="list-group">
 $base['adm_content'] = '';
 $base['adm_actual'] = 'Sie sind hier: ';
 
-
+// config.ini for heinercms
+$config_ini = __DIR__ . '/../inc/config.ini';
 
 
