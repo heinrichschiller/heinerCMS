ea7bda32f51250543db7d0560be3e9d18f0bf5e3
Heinrich Schiller <heinrich@heinrich-schiller.de>
2017-05-09 08:28:02 +0200

    new

---
+++ templates/dashboard.php
@@ -0,0 +1,51 @@
+<div class="container">
+<div class="row">
+	<div class="col-lg-10 col-md-10 col-sm-10 col-12">
+		<div class="panel">
+			<h4><span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span> Dashboard</h4>
+		</div>
+	</div>
+</div>
+	<div class="row">
+		<div class="col-lg-5 col-md-4 col-sm-5 col-xs-12">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">News</h3>
+				</div>
+				<div class="panel-body">Sie haben noch keine News.</div>
+				<div class="panel-body"><a href="?uri=newsadd">Anlegen</a></div>
+			</div>
+		</div>
+		<div class="col-lg-5 col-md-4 col-sm-5 col-xs-12">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Downloads</h3>
+				</div>
+				<div class="panel-body">Sie haben noch keine Downloads.</div>
+				<div class="panel-body"><a href="?uri=downloadsadd">Anlegen</a></div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-5 col-md-4 col-sm-5 col-xs-12">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Links</h3>
+				</div>
+				<div class="panel-body">Sie haben noch keine Links.</div>
+				<div class="panel-body"><a href="?uri=linksadd">Anlegen</a></div>
+			</div>
+		</div>
+		<div class="col-lg-5 col-md-4 col-sm-5 col-xs-12">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Articles</h3>
+				</div>
+				<div class="panel-body">Sie haben noch keine Artikel</div>
+				<div class="panel-body"><a href="?uri=articlesadd">Anlegen</a></div>
+			</div>
+		</div>
+	</div>
+</div>
