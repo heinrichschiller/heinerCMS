2cc5a6afc23ca4682d88cf2c3624becd196df818
heinrichschiller <heinrich@heinrich-schiller.de>
2017-09-16 14:03:36 +0200

    update

--- admin/articleinsert.php
+++ admin/articleinsert.php
@@ -4,20 +4,32 @@ include __DIR__ . '/../inc/base.inc.php';
 include __DIR__ . '/../inc/functions.inc.php';
 include __DIR__ . '/../inc/login.inc.php';
 
-/* Überprüfen ob Login erfolgt ist, ggf. Anmeldemöglichkeit bieten */
-if (is_logged_in ()) {
-	$id = filter_input(INPUT_POST, 'id');
-	$title = filter_input(INPUT_POST, 'title');
-	$content = filter_input(INPUT_POST, 'content');
-	$visible = filter_input(INPUT_POST, 'visible');
-	
-	$con = getDB();
+require __DIR__ . '/../source/controllers/articles/ArticleController.php';
+require __DIR__ . '/../source/models/articles/ArticleModel.php';
 
-	if ($con) {
-		$sql = "INSERT INTO articles (title,content,visible) VALUES ('$title','$content',$visible)";
-		$result = mysqli_query ( $con, $sql );
-		header ( 'Location: index.php?uri=articles' );
-	}
-	mysqli_close( $con );
+/* Überprüfen ob Login erfolgt ist, ggf. Anmeldemöglichkeit bieten */
+
+$con = getDB();
+
+if (is_logged_in ()) {
+    
+    $model = new ArticleModel();
+    $article = new ArticleController($con, $model);
+    
+    $model->setId(filter_input(INPUT_POST, 'id'));
+    $model->setTitle(filter_input(INPUT_POST, 'title'));
+    $model->setContent(filter_input(INPUT_POST, 'content'));
+    $model->setVisible(filter_input(INPUT_POST, 'visible'));
+
+
+    try {
+        
+        $article->add();
+        
+    } catch (Exception $ex) {
+        echo $ex->getMessage();
+    }
+    
+    header ( 'Location: index.php?uri=articles' );
 }
 
