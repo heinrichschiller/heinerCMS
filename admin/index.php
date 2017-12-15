e96c44de8b1bdb21e7721ecf85ae650e42456c5f
Heinrich Schiller <heinrich@heinrich-schiller.de>
2017-12-15 16:06:19 +0100

    update

--- admin/index.php
+++ admin/index.php
@@ -16,16 +16,33 @@ include __DIR__ . '/../source/views/user/UserListView.php';
 $uri = filter_input ( INPUT_GET, 'uri' );
 $id = filter_input ( INPUT_GET, 'id' );
 
-/* Einfaches Logging zu demonstationszwecken UND ONHE VERSCHLUESSELUNG. */
+/* Einfaches Login zu demonstrationszwecken UND ONHE VERSCHLUESSELUNG. */
 if (is_logged_in ()) {
 	
     $config = __DIR__ . '/../source/configs/config.ini';
+    $xmlfile = __DIR__ . '/../data/locales/de.xml';
     
     if (file_exists($config)) {
         $ini_array = parse_ini_file($config);
         $_SESSION['theme'] = $ini_array['theme'];
     }
 
+    $xmlString = file_get_contents($xmlfile);
+    $xml = simplexml_load_string($xmlString);
+
+    $arr_keys = [];
+    $arr_values = [];
+    $arr_language = [];
+    
+    foreach ($xml->children() as $second_gen) {
+        foreach ($second_gen->children() as $third_gen) {
+            array_push( $arr_keys, '{'.$third_gen->getName().'}');
+            array_push( $arr_values, (string)$third_gen);
+        }
+    }
+
+    $arr_language = array_combine($arr_keys, $arr_values);
+    
 	/* Template einlesen */
 	$template = loadTemplate( 'adm_template' );
 	
@@ -40,7 +57,7 @@ if (is_logged_in ()) {
 	$template = str_replace ( '$PHP_SELF', $_SERVER ['PHP_SELF'], $template );
 	
 	/* translation */
-	$template = strtr($template, $arr_language_de);
+	$template = strtr($template, $arr_language);
 	
 	/* Template ausgeben */
 	echo stripslashes ( $template );
