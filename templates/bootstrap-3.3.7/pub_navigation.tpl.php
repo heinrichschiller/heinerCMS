fc918b8c2a6541429a0115496bed879af1937b1d
heinrichschiller <heinrich@heinrich-schiller.de>
2018-01-18 18:55:39 +0100

    new

---
+++ templates/bootstrap-3.3.7/pub_navigation.tpl.php
@@ -0,0 +1,5 @@
+<li><a href="<@url@>" target="blank">Blog</a></li>
+<li><a href="$PHP_SELF?uri=news">News</a></li>
+<li><a href="$PHP_SELF?uri=downloads">Downloads</a></li>
+<li><a href="$PHP_SELF?uri=links">Links</a></li>
+<li><a href="$PHP_SELF?uri=articles">Artikel</a></li>
