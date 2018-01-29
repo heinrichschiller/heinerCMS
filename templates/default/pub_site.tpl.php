1f4466b60e39125f63f1500d0f48e0763a68359e
heinrichschiller <heinrich@heinrich-schiller.de>
2018-01-29 18:07:56 +0100

    new

---
+++ templates/default/pub_site.tpl.php
@@ -0,0 +1,15 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4"><@placeholder-title@></h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+</div>
+<div class="row">
+	<div class="col-sm-2">
+	</div>
+	<div class="col-sm-8">
+		<@placeholder-content@>
+	</div>
+	<div class="col-sm-2">
+	</div>
+</div>
