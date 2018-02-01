1099570cd7c9fdc5626a68beb132fac3e8030897
heinrichschiller <heinrich@heinrich-schiller.de>
2018-02-01 15:56:19 +0100

    new, added mainpage

---
+++ templates/default/pub_mainpage.tpl.php
@@ -0,0 +1,206 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Heinrich-Schiller.de</h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+</div>
+<div class="container">
+<div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="data/images/teaser/teaser-1.jpg" title="Teaser 1" alt="Teaser 1" />
+                    <div class="card-body">
+                        <span class="card-title">Überschrift eins</span>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-teaser">Weiterlesen</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="data/images/teaser/teaser-2.jpg" title="Teaser 2" alt="Teaser 2" />
+                    <div class="card-body">
+                        <span class="card-title">Überschrift zwei</span>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-teaser">Weiterlesen</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="data/images/teaser/teaser-3.jpg" title="Teaser 3" alt="Teaser 3" />
+                    <div class="card-body">
+                        <span class="card-title">Überschrift drei</span>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-teaser">Weiterlesen</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <div class="content">
+        <div class="row">
+            <article class="col-md-8">
+                <h2>Ein Interessanter Newsartikel</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
+
+                <p>Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit quam beatae tempora quibusdam illum! Modi velit odio nam nulla unde amet odit pariatur at! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas!</p>
+
+                <a class="btn readmore" href="#">Weiterlesen</a>
+                <a class="btn comments" href="#">Kommentare <span class="badge badge-secondary">20</span></a>
+                
+            </article>
+            
+            <aside class="col-md-4">
+                <div class="alert alert-success">
+                    <h4 class="alert-heading">Well done!</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
+                        <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+                </div>
+            </aside> 
+        </div>
+	</div>   
+	
+	
+	<div class="sliderbox">
+    <div class="container">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="data/images/slider/slide-1.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="data/images/slider/slide-2.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="data/images/slider/slide-2.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="data/images/slider/slide-2.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="data/images/slider/slide-2.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="data/images/slider/slide-2.jpg" alt="Second slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Zurück</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Weiter</span>
+            </a>
+        </div>
+    </div>
+</div>    
+<div class="container">
+    <div class="content">
+        <div class="row">
+            <div class="col-md-6">
+                <div id="accordion" role="tablist">
+                    <div class="card">
+                        <div class="card-header" role="tab" id="one">
+                            <div class="mb-0 h5">
+                                <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    Kontaktdaten
+                                </a>
+                            </div>
+                        </div>
+
+                        <div id="collapseOne" class="collapse show" role="tabpanel" data-parent="#accordion">
+                            <div class="card-body">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" role="tab" id="two">
+                            <div class="mb-0 h5">
+                                <a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="true">
+                                    Social Media
+                                </a>
+                            </div>
+                        </div>
+
+                        <div id="collapseTwo" class="collapse" role="tabpanel" data-parent="#accordion">
+                            <div class="card-body">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" role="tab" id="three">
+                            <div class="mb-0 h5">
+                                <a class="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseTwo">
+                                    Ihr Ansprechpartner
+                                </a>
+                            </div>
+                        </div>
+                        <div id="collapseThree" class="collapse" role="tabpanel" data-parent="#accordion">
+                            <div class="card-body">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" role="tab" id="four">
+                            <div class="mb-0 h5">
+                                <a class="collapsed" data-toggle="collapse" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                    Öffnungszeiten
+                                </a>
+                            </div>
+                        </div>
+                        <div id="collapseFour" class="collapse" role="tabpanel" data-parent="#accordion">
+                            <div class="card-body">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Vor- und Nachname">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="exampleFormControlInput2" placeholder="name@example.com">
+                    </div>
+                    <div class="form-group">
+                        <input type="tel" class="form-control" id="exampleFormControlInput3" placeholder="Telefon">
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Nachricht" rows="3"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary submit">Absenden</button>
+
+                        <button type="button" class="btn btn-primary submit" data-toggle="modal" data-target="#exampleModal">
+                            Speichern
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>    
+    <div class="row">
+    	<div class="col-sm-12">
+    		<@placeholder_mainpage@>
+    	</div>
+    </div>
+</div>
