cce6fe4806cb1349c827f9999422088a36d224b0
heinrichschiller <heinrich@heinrich-schiller.de>
2018-02-15 11:45:46 +0100

    updates

--- templates/classic/adm_general_settings.tpl.php
+++ templates/classic/adm_general_settings.tpl.php
@@ -8,7 +8,7 @@
 				<label for="title">{title}:</label>
 			</td>
 			<td>
-				<input type="text" name="title" value="<?= $_SESSION['title']; ?>">
+				<input type="text" name="title" value="<@title@>">
 			</td>
 		</tr>
 		<tr>
@@ -16,11 +16,7 @@
 				<label for="sel1">Themes:</label>
 			</td>
 			<td>
-				<select class="form-control" name="theme">
-                  <?php foreach ($result as $theme) :?>
-             		   <option><?= $theme;?></option>
-             	 <?php endforeach;?>
-                  </select>
+				<@placeholder-select-theme@>
             </td>
 		</tr>
 		<tr>
