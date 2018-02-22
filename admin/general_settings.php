7879c3114170701fd92443f68abb326688d1523b
Heinrich Schiller <heinrich@heinrich-schiller.de>
2018-02-22 21:38:00 +0100

    update

--- admin/general_settings.php
+++ admin/general_settings.php
@@ -1,42 +1,40 @@
 <?php
 
-$filename = __DIR__ . '/../source/configs/config.ini';
+include __DIR__ . '/../cms-config.php';
 
-if (file_exists($filename)) {
-    $ini_array = parse_ini_file($filename);
-} else {
-    echo 'File .config.ini not found.';
-    exit();
+include __DIR__ . '/../include/general_functions.inc.php';
+include __DIR__ . '/../include/login.inc.php';
+
+if (is_logged_in()) {
+    
+    $title = filter_input(INPUT_POST, 'title');
+    $theme = filter_input(INPUT_POST, 'theme');
+    $tagline = filter_input(INPUT_POST, 'tagline');
+    $blogUrl = filter_input(INPUT_POST, 'blogUrl');
+    
+    $pdo = getPdoDB();
+    
+    $sql = 'INSERT INTO `settings`(`title`, `tagline`, `theme`, `blog_url`)'
+        . ' VALUES (:title, :tagline, :theme, :blog_url)';
+        
+    $input_parameters = [
+        ':title'   => $title,
+        ':tagline' => $tagline,
+        ':theme' => $theme,
+        ':blog_url' => $blogUrl
+    ];
+
+    try {
+            
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($input_parameters);
+            
+    } catch (Exception $ex) {
+            
+        echo $ex->getMessage();
+        exit();
+            
+    }
+    
+    header ( "Location: index.php?uri=general" );
 }
-
-$title = filter_input(INPUT_POST, 'title');
-$theme = filter_input(INPUT_POST, 'theme');
-$tagline = filter_input(INPUT_POST, 'tagline');
-$blogUrl = filter_input(INPUT_POST, 'blogUrl');
-
-$host = $ini_array['host'];
-$user = $ini_array['user'];
-$password = $ini_array['password'];
-$database = $ini_array['database'];
-
-$fp = fopen($filename, 'w');
-
-fwrite($fp, "[Application]\n");
-fwrite($fp, "title = $title\n");
-fwrite($fp, "tagline = $tagline\n");
-fwrite($fp, "theme = $theme\n");
-fwrite($fp, "version = 0.0.1");
-fwrite($fp, "\n\n");
-fwrite($fp, "[optional]\n");
-fwrite($fp, "blog_url = $blogUrl\n");
-fwrite($fp, "\n\n");
-fwrite($fp, "[Database]\n");
-fwrite($fp, "type = mysql\n");
-fwrite($fp, "host = \n");
-fwrite($fp, "user = $user\n");
-fwrite($fp, "password = $password\n");
-fwrite($fp, "database = $database\n");
-
-fclose($fp);
-
-header ( "Location: index.php?uri=general" );
