eb2eedc6dcec7b29f45dd3d177ca48498bb2d33d
heinrichschiller <heinrich@heinrich-schiller.de>
2018-02-26 15:40:10 +0100

    update, improve admin navigation

--- templates/default/adm_navigation.tpl.php
+++ templates/default/adm_navigation.tpl.php
@@ -1,10 +1,8 @@
 <!-- Dashboard -->
 <div class="list-group spacing">
-	<li class="list-group-item">
-        <a href="$PHP_SELF?uri=dashboard">
+        <a href="$PHP_SELF?uri=dashboard" class="list-group-item list-group-item-action active">
             <img class="glyph-icon-16" src="../templates/default/img/svg/si-glyph-dashboard.svg"> {dashboard}
         </a>
-    </li>
 </div>
 
 <!-- Content -->
@@ -12,42 +10,30 @@
     <li class="list-group-item active">
         <span class="glyphicon glyphicon-book" aria-hidden="true"></span> {content}
     </li>
-    <li class="list-group-item">
-        <a href="$PHP_SELF?uri=news">
+        <a href="$PHP_SELF?uri=news" class="list-group-item list-group-item-action">
             <img class="glyph-icon-16" src="../templates/default/img/svg/si-glyph-pen.svg"> {news}
             <span class="badge badge-light">##placeholder-news##</span>
         </a>
-    </li>
-    <li class="list-group-item">
-        <a href="$PHP_SELF?uri=downloads">
+        <a href="$PHP_SELF?uri=downloads" class="list-group-item list-group-item-action">
             <img class="glyph-icon-16" src="../templates/default/img/svg/si-glyph-file-download.svg"> {downloads}
             <span class="badge badge-light">##placeholder-downloads##</span>
         </a>
-    </li>
-    <li class="list-group-item">
-        <a href="$PHP_SELF?uri=links">
+        <a href="$PHP_SELF?uri=links" class="list-group-item list-group-item-action">
             <img class="glyph-icon-16" src="../templates/default/img/svg/si-glyph-global.svg"> {links}
             <span class="badge badge-light">##placeholder-links##</span>
         </a>
-    </li>
-    <li class="list-group-item">
-        <a href="$PHP_SELF?uri=articles">
+        <a href="$PHP_SELF?uri=articles" class="list-group-item list-group-item-action">
             <img class="glyph-icon-16" src="../templates/default/img/svg/si-glyph-pen.svg"> {articles}
             <span class="badge badge-light">##placeholder-articles##</span>
         </a>
-    </li>
-    <li class="list-group-item">
-        <a href="$PHP_SELF?uri=sites">
+        <a href="$PHP_SELF?uri=sites" class="list-group-item list-group-item-action">
             <img class="glyph-icon-16" src="../templates/default/img/svg/si-glyph-document.svg"> {sites}
             <span class="badge badge-light">##placeholder-sites##</span>
         </a>
-    </li>
-    <li class="list-group-item">
-        <a href="$PHP_SELF?uri=trash">
+        <a href="$PHP_SELF?uri=trash" class="list-group-item list-group-item-action">
             <img class="glyph-icon-16" src="../templates/default/img/svg/si-glyph-trash.svg"> {trash}
             <span class="badge badge-light">##placeholder-trash##</span>
         </a>
-    </li>
 </div>
 
 <!-- Communication -->
@@ -55,12 +41,10 @@
     <li class="list-group-item active">
         <span class="glyphicon glyphicon-book" aria-hidden="true"></span> {communication}
     </li>
-    <li class="list-group-item">
-        <a href="$PHP_SELF?uri=news">
+        <a href="$PHP_SELF?uri=news" class="list-group-item list-group-item-action">
             <img class="glyph-icon-16" src="../templates/default/img/svg/si-glyph-contact-book.svg"> {contact_inquiry}
             <span class="badge badge-light">0<!-- ##placeholder-inqiery##  --></span>
         </a>
-    </li>
 </div>
 
 <!-- Settings -->
@@ -68,24 +52,16 @@
     <li class="list-group-item active">
         <img class="glyph-icon-24" src="../templates/default/img/svg/si-glyph-gear-1.svg"> {settings}
     </li>
-    <li class="list-group-item">
-        <a href="$PHP_SELF?uri=general">
+        <a href="$PHP_SELF?uri=general" class="list-group-item list-group-item-action">
             <img class="glyph-icon-16" src="../templates/default/img/svg/si-glyph-gear.svg"> {general}
         </a>
-    </li>
-    <li class="list-group-item">
-        <a href="$PHP_SELF?uri=mainpage">
+        <a href="$PHP_SELF?uri=mainpage" class="list-group-item list-group-item-action">
             <img class="glyph-icon-16" src="../templates/default/img/svg/si-glyph-home-page.svg"> {mainpage}
         </a>
-    </li>
-    <li class="list-group-item">
-        <a href="$PHP_SELF?uri=linksettings">
+        <a href="$PHP_SELF?uri=linksettings" class="list-group-item list-group-item-action">
             <img class="glyph-icon-16" src="../templates/default/img/svg/si-glyph-global.svg"> {links}
         </a>
-    </li>
-    <li class="list-group-item">
-        <a href="$PHP_SELF?uri=user">
+        <a href="$PHP_SELF?uri=user" class="list-group-item list-group-item-action">
             <img class="glyph-icon-16" src="../templates/default/img/svg/si-glyph-person-people.svg"> {user}
         </a>
-    </li>
 </div>
