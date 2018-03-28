aa956d7251ccab655b886fe6d35f738a72572f99
Heinrich Schiller <heinrich@heinrich-schiller.de>
2018-03-28 16:48:22 +0200

    update, template

--- _installer_/templates/final.tpl.php
+++ _installer_/templates/final.tpl.php
@@ -1,21 +1,21 @@
 <div class="container">
-    	<header>
-        	<h2>Ende</h2>
+    <header>
+        <h2>{header_8}</h2>
     </header>
     <div class="row">
 		<div class="col-md-12">
-			<p>Gratulation! Sie haben es beinahe geschafft :-)</p>
-			<p>Zum Schluss können sie mit drücken auf <strong>Weiter</strong> die Software nun nutzen.</p>
-			<p>Bitte löschen sie im Anschluss den Ordner <strong>_installer_</strong> sobald sie sicher sind das alles korrekt funktioniert.</p>
-			<p>Somit wird sicher gestellt das kein weiterer Nutzer, eine neue Installation machen kann.</p>
+			<p>{text_35}</p>
+			<p>{text_36} <strong>{next}</strong> {text_37}</p>
+			<p>{text_38} <strong>_installer_</strong> {text_39}</p>
+			<p>{text_40}</p>
 			<br>
-			<p>Sie können sich auch das Installationsprotokoll ansehen um sicher zu gehen das alles korrekt eingerichtet wurde. <br>
-			<a href="protocol.php" target="blank">Protokoll anzeigen</a></p>
+			<p>{text_41} <br>
+			<a href="protocol.php" target="blank">{show_protocol}</a></p>
 		</div>
     </div>
     <div class="row button_bottom">
-        	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        		<a href="final.php" class="btn btn-primary" role="button">Weiter</a>
-        	</div>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+       		<a href="final.php" class="btn btn-primary" role="button">{next}</a>
+       	</div>
     </div>
 </div>
