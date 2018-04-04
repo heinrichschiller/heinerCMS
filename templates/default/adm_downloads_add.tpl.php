a11464954400aa62675eee5c36d785be9097e7d6
heinrichschiller <heinrich@heinrich-schiller.de>
2018-04-04 14:31:58 +0200

    update

--- templates/default/adm_downloads_add.tpl.php
+++ templates/default/adm_downloads_add.tpl.php
@@ -5,7 +5,7 @@
    		</div>
    	</div>
 </div>
-<form action="downloadsinsert.php" method="post">
+<form action="downloadsinsert.php" method="post" enctype="multipart/form-data">
 	<div class="row">
 		<div class="col-lg-8 col-md-9 col-sm-9 col-xs-12">
 			<div class="form-group">
@@ -43,15 +43,19 @@
 			</div>
 		</div>
 	</div>
-	<div class="input-group mb-3">
-  <div class="custom-file">
-    <input type="file" class="custom-file-input" id="inputGroupFile02">
-    <label class="custom-file-label" for="inputGroupFile02">Choose file</label>
-  </div>
-  <div class="input-group-append">
-    <span class="input-group-text" id="">Upload</span>
-  </div>
-</div>
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        	<div class="input-group mb-3">
+                <div class="custom-file">
+                    <input type="file" name="file" class="custom-file-input" id="inputGroupFile02">
+                    <label class="custom-file-label" for="inputGroupFile02"></label>
+                </div>
+                <div class="input-group-append">
+                    <span class="input-group-text" id="">Upload</span>
+                </div>
+        	</div>
+        </div>
+    </div>
 	<div class="row">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 			<div class="form-group">
