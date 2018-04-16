12575d70c6910fbe66abf9d35e2c9ea78a2c2c50
heinrichschiller <heinrich@heinrich-schiller.de>
2018-04-16 15:30:48 +0200

    added, new file pdo_db_functions.

--- include/general_functions.inc.php
+++ include/general_functions.inc.php
@@ -56,62 +56,6 @@ function get_translation(string $language) : array
 }
 
 /**
- * Database adapter for MySQL
- *
- * @return PDO
- */
-function getPdoDB()
-{   
-    try {
-        
-        $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
-        
-        return $pdo;
-        
-    } catch (PDOException $ex) {
-        echo $ex->getMessage();
-        exit();
-    }
-    
-}
-
-/**
- *
- * @param string $sql
- * @param array $params
- * @return array
- */
-function pdo_select(string $sql, array $params) : array
-{
-    $pdo = getPdoDB();
-    
-    try {
-        $stmt = $pdo->prepare($sql);
-        
-        if (!empty($params)) {
-            $stmt->execute( [$params[0] ]);
-        } else {
-            $stmt->execute();
-        }
-        
-    } catch (PDOException $ex) {
-        echo $ex->getMessage();
-    }
-    
-    return $stmt->fetch();
-}
-
-/**
- *
- * @param string $sql
- * @param array $params
- */
-function pdo_query(string $sql, array $params)
-{
-    $pdo = getPdoDB();
-}
-
-/**
  *
  * @param string $template
  * @return string
