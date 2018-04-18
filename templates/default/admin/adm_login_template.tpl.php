70df0d812f1d080d5973efaec4a1b540ace394d8
Heinrich Schiller <heinrich@heinrich-schiller.de>
2018-04-18 20:07:37 +0200

    rename

---
+++ templates/default/admin/adm_login_template.tpl.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>##placeholder-title##</title>
+		<meta charset="utf8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="../vendor/components/jquery/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="../vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+		<link href="../templates/default/admin/css/signin_style.css" rel="stylesheet">
+	</head>
+	<body class="text-center">
+        ##placeholder-form-signin##
+	</body>
+</html>
