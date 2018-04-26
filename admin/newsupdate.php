609daa69533440c5f8ad475801ca33a577f35722
heinrichschiller <heinrich@heinrich-schiller.de>
2018-04-26 17:14:36 +0200

    update, refactory, clean code, fixed bugs

--- admin/newsupdate.php
+++ admin/newsupdate.php
@@ -1,42 +1,37 @@
 <?php
 
+session_start();
+
 include __DIR__ . '/../cms-config.php';
 
 include __DIR__ . '/../include/pdo_db_functions.inc.php';
 include __DIR__ . '/../include/general_functions.inc.php';
-include __DIR__ . '/../include/login.inc.php';
 
 if (is_logged_in ()) {
-    
-	$id      = filter_input(INPUT_POST, 'id');
-	$title   = filter_input(INPUT_POST, 'title');
-	$message = filter_input(INPUT_POST, 'message');
-	$visible = filter_input(INPUT_POST, 'visible');
 
-	$pdo = getPdoDB();
+    $id      = filter_input(INPUT_POST, 'id');
+    $title   = filter_input(INPUT_POST, 'title');
+    $message = filter_input(INPUT_POST, 'message');
+    $visible = filter_input(INPUT_POST, 'visible');
 
-	$sql = "UPDATE `news` SET `title` = :title, `message` = :message, `visible` = :visible WHERE `id` = :id";
+    $pdo = getPdoConnection();
 
-	$inpuit_parameters = [
-	    ':title'   => $title,
-	    ':message' => $message,
-	    ':visible' => $visible,
-	    ':id'      => $id
-	];
-	
-	try {
-	    
-	    $stmt = $pdo->prepare($sql);
-	    $stmt->execute($inpuit_parameters);
-	    
-	} catch (PDOException $ex) {
-	    
-	    echo $ex->getMessage();
-	    exit();
-	    
+    $sql = "UPDATE `news` SET `title` = :title, `message` = :message, `visible` = :visible WHERE `id` = :id";
+
+    try {
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':message', $message);
+        $stmt->bindParam(':visible', $visible);
+        $stmt->bindParam(':id', $id);
+
+        $stmt->execute($inpuit_parameters);
+
+    } catch (PDOException $ex) {
+        echo $ex->getMessage();
+        exit();
 	}
-	
-	header ( 'Location: index.php?uri=news');
 
+    header ('Location: index.php?uri=news');
 }
-
