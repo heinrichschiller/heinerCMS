a47c64fea52e418288ba2e991f41a14848bbdde0
heinrichschiller <heinrich@heinrich-schiller.de>
2018-04-26 17:12:23 +0200

    update, removed links

--- templates/default/public/pub_downloads_content.tpl.php
+++ templates/default/public/pub_downloads_content.tpl.php
@@ -1,12 +1,9 @@
 <div class="row">
 	<div class="col-md-12">
 		<ul class="list-group">
-			<li class="list-group-item active"><a
-				href="##placeholder-links-uri##" target="blank">##placeholder-downloads-title##
-					- ##placeholder-downloads-datetime##</a></li>
+			<li class="list-group-item active">##placeholder-downloads-title## - ##placeholder-downloads-datetime##</li>
 			<li class="list-group-item">##placeholder-downloads-comment##</li>
-			<li class="list-group-item"><a
-				href="##placeholder-downloads-path##/##placeholder-downloads-filename##">Download</a></li>
+			<li class="list-group-item"><a href="##placeholder-downloads-path##/##placeholder-downloads-filename##">Download</a></li>
 		</ul>
 	</div>
 </div>
