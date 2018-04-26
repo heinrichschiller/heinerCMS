4f01b9949a1b4c762c57acf80e7be26ef23d07b0
heinrichschiller <heinrich@heinrich-schiller.de>
2018-04-26 17:15:12 +0200

    update, refactory, clean code, fixed bugs

--- admin/downloadsupdate.php
+++ admin/downloadsupdate.php
@@ -1,46 +1,43 @@
 <?php
 
+session_start();
+
 include __DIR__ . '/../cms-config.php';
 
 include __DIR__ . '/../include/pdo_db_functions.inc.php';
 include __DIR__ . '/../include/general_functions.inc.php';
-include __DIR__ . '/../include/login.inc.php';
 
 /* Überprüfen ob Login erfolgt ist, ggf. Anmeldemöglichkeit bieten */
 if (is_logged_in ()) {
-    
-	$id       = filter_input(INPUT_POST, 'id');
-	$title    = filter_input(INPUT_POST, 'title');
-	$comment  = filter_input(INPUT_POST, 'comment');
-	$path     = filter_input(INPUT_POST,'path');
-	$filename = filter_input(INPUT_POST,'filename');
-	$visible  = filter_input(INPUT_POST,'visible');
-	
-    $pdo = getPdoDB();
-    
-    $sql = "UPDATE downloads SET title = :title, comment = :comment, path = :path, filename = :filename, visible = :visible WHERE id = :id";
-    
-    $input_parameters = [
-        ':title'    => $title,
-        ':comment'  => $comment,
-        ':path'     => $path,
-        ':filename' => $filename,
-        ':visible'  => $visible,
-        ':id'       => $id
-    ];
-    
+
+    $id       = filter_input(INPUT_POST, 'id');
+    $title    = filter_input(INPUT_POST, 'title');
+    $comment  = filter_input(INPUT_POST, 'comment');
+    $path     = filter_input(INPUT_POST,'path');
+    $filename = filter_input(INPUT_POST,'filename');
+    $visible  = filter_input(INPUT_POST,'visible');
+
+    $pdo = getPdoConnection();
+
+    $sql = "UPDATE `downloads` SET `title` = :title, `comment` = :comment, `path` = :path, `filename` = :filename, `visible` = :visible 
+        WHERE `id` = :id";
+
     try {
-        
         $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':comment', $comment);
+        $stmt->bindParam(':path', $path);
+        $stmt->bindParam(':filename', $filename);
+        $stmt->bindParam(':visible', $visible);
+        $stmt->bindParam(':id', $id);
+
         $stmt->execute($input_parameters);
-        
     } catch (PDOException $ex) {
-        
         echo $ex->getMessage();
         exit();
-        
     }
-    
+
     header('Location: index.php?uri=downloads');
 
 }
