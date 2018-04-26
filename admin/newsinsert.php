6d832a08d055204f97e4abbfa10b4b62c6ae35c4
heinrichschiller <heinrich@heinrich-schiller.de>
2018-04-26 18:33:01 +0200

    update, clean code

--- admin/newsinsert.php
+++ admin/newsinsert.php
@@ -9,44 +9,41 @@ include __DIR__ . '/../include/general_functions.inc.php';
 
 /* Überprüfen ob Login erfolgt ist, ggf. Anmeldemöglichkeit bieten */
 if (is_logged_in()) {
-    
+
     $pdo = getPdoConnection();
-    
+
     $title   = filter_input(INPUT_POST, 'title');
     $message = filter_input(INPUT_POST, 'message');
     $visible = filter_input(INPUT_POST, 'visible');
-    
+
     $sql = 'INSERT INTO `news` (`title`, `message`, `visible`)'
         . " VALUES (:title, :message, :visible)";
-    
+
     if (DB_DRIVER == 'sqlite') {
         $datetime = strftime('%Y-%m-%d %H:%M', time());
         $trash = 'false';
-        
+
         $sql = 'INSERT INTO `news` (`title`, `message`, `created_at`, `visible`, `trash`)'
             . " VALUES (:title, :message, :created_at, :visible, :trash)";
     }
 
     try {
         $stmt = $pdo->prepare($sql);
-        
+
         $stmt->bindParam(':title', $title);
         $stmt->bindParam(':message', $message);
         $stmt->bindParam(':visible', $visible);
-        
+
         if (DB_DRIVER == 'sqlite') {
             $stmt->bindParam(':created_at', $datetime);
             $stmt->bindParam(':trash', $trash);
         }
-        
+
         $stmt->execute();
     } catch (PDOException $ex) {
-        
         echo $ex->getMessage();
         exit();
-        
     }
-    
+
     header('Location: index.php?uri=news');
 }
-
