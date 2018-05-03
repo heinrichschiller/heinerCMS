8b4f1ff87726250b25e1e1cad22fb717389cbd00
heinrichschiller <heinrich@heinrich-schiller.de>
2018-05-03 13:57:54 +0200

    update, clean code

--- admin/userdel.php
+++ admin/userdel.php
@@ -1,32 +1,32 @@
 <?php
 
+session_start();
+
 include __DIR__ . '/../cms-config.php';
 
 include __DIR__ . '/../include/pdo_db_functions.inc.php';
 include __DIR__ . '/../include/general_functions.inc.php';
-include __DIR__ . '/../include/login.inc.php';
 
 /* Überprüfen ob Login erfolgt ist, ggf. Anmeldemöglichkeit bieten */
 if (is_logged_in()) {
-    
+
     $id     = filter_input(INPUT_POST, 'id');
     $action = filter_input(INPUT_POST, 'action');
-    
-    $pdo = getPdoDB();
-    
+
+    $pdo = getPdoConnection();
+
     $sql = 'DELETE FROM `users` WHERE id = :id';
-    
+
     try {
-        
         $stmt = $pdo->prepare($sql);
-        $stmt->execute(array(':id' => $id));
-        
+
+        $stmt->bindParam(':id', $id);
+
+        $stmt->execute();
     } catch (PDOException $ex) {
-        
         echo $ex->getMessage();
         exit();
-        
     }
-    
+
     header ( "Location: index.php?uri=user" );
 }
