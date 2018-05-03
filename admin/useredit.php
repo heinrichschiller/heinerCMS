0260ccc66d3de1c0a6de9eedbf78faaed8673c82
heinrichschiller <heinrich@heinrich-schiller.de>
2018-05-03 14:09:02 +0200

    update, clean code

--- admin/useredit.php
+++ admin/useredit.php
@@ -1,10 +1,11 @@
 <?php
 
+session_start();
+
 include __DIR__ . '/../cms-config.php';
 
 include __DIR__ . '/../include/pdo_db_functions.inc.php';
 include __DIR__ . '/../include/general_functions.inc.php';
-include __DIR__ . '/../include/login.inc.php';
 
 if (is_logged_in()) {
 
@@ -16,37 +17,28 @@ if (is_logged_in()) {
     $publicAs  = filter_input(INPUT_POST, 'public_as');
     $active    = filter_input(INPUT_POST, 'active');
 
-    $pdo = getPdoDB();
+    $pdo = getPdoConnection();
 
-    $sql = 'UPDATE `users` SET `id`= :id
-        ,`firstname`= :firstname
+    $sql = 'UPDATE `users` SET `firstname`= :firstname
         ,`lastname`= :lastname
         ,`email`= :email
         ,`username`= :username
-        ,`public_as`= :public_as
         ,`active`= :active';
-    
-    $input_parameters = [
-        ':id'        => $id,
-        ':firstname' => $firstname,
-        ':lastname'  => $lastname,
-        ':email'     => $email,
-        ':username'  => $username,
-        ':public_as' => $publicAs,
-        ':active'    => $active
-    ];
-        
-        try {
 
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute($input_parameters);
-            
-        } catch (PDOException $ex) {
-            
-            echo $ex->getMessage();
-            exit();
-            
-        }
-        
-        header ( 'Location: index.php?uri=user' );
+    try {
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(':firstname', $firstname);
+        $stmt->bindParam(':lastname', $lastname);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':active', $active);
+
+        $stmt->execute();
+    } catch (PDOException $ex) {
+        echo $ex->getMessage();
+        exit();
+    }
+
+    header ( 'Location: index.php?uri=user' );
 }
