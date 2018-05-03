2042cb4b0bdaa880ba4d68368577558a460aee91
heinrichschiller <heinrich@heinrich-schiller.de>
2018-05-03 13:58:59 +0200

    update, update template

--- templates/default/public/pub_navigation.tpl.php
+++ templates/default/public/pub_navigation.tpl.php
@@ -1,8 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<a class="navbar-brand" href="index.php?uri=mainpage">##placeholder-title##</a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse"
-		data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
-		aria-expanded="false" aria-label="Toggle navigation">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
 	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -13,7 +11,7 @@
 			##placeholder-links##
 			##placeholder-articles##
 			##placeholder-pages##
-			<a class="nav-item nav-link" href="index.php?uri=admin">Sign in</a></li>
+			<a class="nav-item nav-link" href="index.php?uri=admin">Sign in</a>
 		</div>
 	</div>
 </nav>
