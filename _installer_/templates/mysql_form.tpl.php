9d555b35e7a2b5eaa9dec6deb53937ace86543ba
heinrichschiller <heinrich@heinrich-schiller.de>
2018-05-04 16:35:58 +0200

    added, new templates for mysql and sqlite

---
+++ _installer_/templates/mysql_form.tpl.php
@@ -0,0 +1,23 @@
+    <form method="post" action="databaseinsert.php?lang=##placeholder-lang##">
+    	<div class="form-group">
+    		<input type="text" name="address" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="{address}" required>
+    	</div>
+    	<div class="form-group">
+    		<input type="text" name="user" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="{user}" required>
+    	</div>
+    	<div class="form-group">
+    		<input type="text" name="database" class="form-control" id="exampleInputPassword1" placeholder="{database}" required>
+    		<input type="checkbox" name="new_db" value="1"><small> {text_18} *</small>
+    	</div>
+    	<div class="form-group">
+    		<input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="{password}">
+    	</div>
+    	<button type="submit" class="btn btn-primary btn_bottom">{next}</button>
+    </form>
+</div>
+<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+	<p>{text_19}</p>
+	<p>{text_20}</p>
+	<p>{text_21} * <small>{text_22}</small></p>
+	<p>{text_23}</p>
+</div>
---
+++ _installer_/templates/sqlite_form.tpl.php
@@ -0,0 +1,5 @@
+<form method="post" action="databaseinsert.php?lang=##placeholder-lang##">
+<p>Wenn sie SQLite ausgewählt haben, werden keine zugangdaten für eine Datenbank benötigt.</p>
+<p>Sie können jetzt auf Weiter klicken.</p>
+<button type="submit" class="btn btn-primary btn_bottom">{next}</button>
+</form>
