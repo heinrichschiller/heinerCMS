a98df5edb1d39228f3b5a21fd9e0535a288f4c15
heinrichschiller <heinrich@heinrich-schiller.de>
2018-07-18 16:05:20 +0200

    added, new general table-templates for content lists

---
+++ templates/default/admin/adm_table.tpl.php
@@ -0,0 +1,17 @@
+<div class="row">
+	<div class="col-lg-10 col-md-10 col-sm-10 col-12">
+		<div class="panel">
+			<h4><img class="glyph-icon-24" src="##placeholder-icon##"> ##placeholder-header##</h4>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		##placeholder-content##
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<a href="##placeholder-uri##" class="btn btn-primary" role="button">##placeholder-button##</a>
+	</div>
+</div>
