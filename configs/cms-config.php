f7e775a5a32db37f072ed737176f9784a0e58e42
heinrichschiller <heinrich@heinrich-schiller.de>
2018-07-23 13:44:17 +0200

    new, cms-config for cms configuration

---
+++ configs/cms-config.php
@@ -0,0 +1,38 @@
+<?php
+
+/****************************************************************************************
+ *
+ * The base configuration for heinerCMS.
+ *
+ * This file contains:
+ *
+ * * 
+ * * 
+ *
+ * @since 0.5.0
+ *
+ ***************************************************************************************/
+
+/**
+ * Path to config directory
+ * 
+ * @var string
+ * @since 0.5.0
+ */
+define('CONFIG_PATH', __DIR__ . '/../config/');
+
+/**
+ * Path to include directory
+ * 
+ * @var string
+ * @since 0.5.0
+ */
+define('INCLUDE_PATH', __DIR__ . '/../include/');
+
+/**
+ * Path to locales directory
+ *
+ * @var string
+ * @since 0.5.0
+ */
+define('LOCALES_PATH', __DIR__ . '/../locales/');
