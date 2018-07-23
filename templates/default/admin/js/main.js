925b9a78e71244601e894473149b969e3b0c570b
heinrichschiller <heinrich@heinrich-schiller.de>
2018-07-23 10:42:36 +0200

    update, clean code

--- templates/default/admin/js/main.js
+++ templates/default/admin/js/main.js
@@ -1,16 +1,6 @@
 "use strict";
 
-window.addEventListener("load", function() {
-	let hide = document.getElementById("hide");
-	
-	hide.addEventListener("click", function() {
-		let hidden = document.getElementById("hidden");
-		
-		hidden.classList.toggle("hidden");
-	}, false);
-	
-	$('.carousel').carousel({
-		  interval: 2000
-		})
+window.addEventListener("DOMContentLoaded", function() {
+
 }, false);
 
