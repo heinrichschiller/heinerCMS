a260e63987e4c074e2ce7c6fb767e3f249d60e63
heinrichschiller <heinrich@heinrich-schiller.de>
2018-08-08 21:17:37 +0200

    update, new db include path

--- admin/login.php
+++ admin/login.php
@@ -6,7 +6,7 @@ include __DIR__ . '/../include/general_functions.inc.php';
 include __DIR__ . '/../configs/cms-config.php';
 include __DIR__ . '/../configs/db-config.php';
 
-include __DIR__ . '/../include/pdo_db_functions.inc.php';
+include __DIR__ . '/../include/' . DB_DRIVER . '_db_functions.inc.php';
 
 if (!checkSystem()) {
     header("Location: /../_installer_/index.php?uri=language&lang=en&db=mysql");
@@ -20,7 +20,14 @@ $login_password = filter_input(INPUT_POST, 'password');
 try {
     $pdo = getPdoConnection();
 
-    $sql = "SELECT `id`,`email`,`username`,`password` FROM `users` WHERE email = :email";
+    $sql = "
+    SELECT `id`,
+        `email`,
+        `username`,
+        `password` 
+        FROM `users` 
+        WHERE email = :email
+    ";
 
     $stmt = $pdo->prepare($sql);
     $stmt->execute(array('email' => $email));
