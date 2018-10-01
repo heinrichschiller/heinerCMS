e1a9d116fc54f0d5a48671dd71fbca040c93cc47
heinrich <heinrich@heinrich-schiller.de>
2018-10-01 14:05:12 +0200

    update, refactoring

--- admin/linkdel.php
+++ admin/linkdel.php
@@ -10,25 +10,10 @@ include __DIR__ . '/../include/general_functions.inc.php';
 
 /* Überprüfen ob Login erfolgt ist, ggf. Anmeldemöglichkeit bieten */
 if (is_logged_in ()) {
-    $id = filter_input(INPUT_GET, 'id');
-    $action = filter_input(INPUT_POST, 'action');
+    $id = filter_input(INPUT_POST, 'id');
 
-    // @todo Unsicher!!! Beheben!!!
-    $list = isset($_POST['chk_select']) ? $_POST['chk_select'] : array();
+    setContentsFlagById($id, 'trash');
 
-    $uri = '';
-
-    switch ( $action ) {
-        case 'del' : deleteItemdsById($list, 'links');
-            $uri .= 'trash';
-            break;
-        case 'del_all' : deleteAllTrashItems('links');
-            $uri .= 'trash';
-            break;
-        default: setContentsFlagById($id, 'trash');
-            $uri .= 'links';
-    }
-
-    header ( "Location: index.php?uri=$uri" );
+    header ( "Location: index.php?uri=links" );
 
 }
