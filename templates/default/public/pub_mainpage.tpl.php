027831099c6e0726653b80ac750fdf6bb3f78852
Heinrich Schiller <heinrich@heinrich-schiller.de>
2018-10-09 13:19:17 +0200

    update

--- templates/default/public/pub_mainpage.tpl.php
+++ templates/default/public/pub_mainpage.tpl.php
@@ -14,28 +14,7 @@
 	<!-- Article -->
 	<div class="content">
 		<div class="row">
-			<article class="col-md-8">
-				<h2>Ein Interessanter Newsartikel</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum
-					explicabo reprehenderit optio amet ab temporibus asperiores quasi
-					cupiditate. Voluptatum ducimus voluptates voluptas?</p>
-
-				<p>Tenetur quod quidem in voluptatem corporis dolorum dicta sit
-					pariatur porro quaerat autem ipsam odit quam beatae tempora
-					quibusdam illum! Modi velit odio nam nulla unde amet odit pariatur
-					at! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum
-					explicabo reprehenderit optio amet ab temporibus asperiores quasi
-					cupiditate. Voluptatum ducimus voluptates voluptas!</p>
-
-				<a class="btn readmore" href="#">Weiterlesen</a>
-				<!-- <a
-					class="btn comments" href="#">Kommentare <span
-					class="badge badge-secondary">20</span></a> -->
-
-			</article>
-
+			##placeholder-articles##
 			<aside class="col-md-4">
 				<div class="alert alert-success">
 					<h4 class="alert-heading">##placeholder-aboutme-title##</h4>
