19c1dd9ca51341c62f52e1d4b7dab75561204bef
heinrichschiller <heinrich@heinrich-schiller.de>
2018-10-11 12:02:04 +0200

    added, delete.php

---
+++ admin/delete.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+include __DIR__ . '/../configs/cms-config.php';
+include __DIR__ . '/../configs/db-config.php';
+
+include __DIR__ . '/../include/' . DB_DRIVER . '_db_functions.inc.php';
+include __DIR__ . '/../include/general_functions.inc.php';
+
+/* Überprüfen ob Login erfolgt ist, ggf. Anmeldemöglichkeit bieten */
+if (is_logged_in()) {
+    $id = filter_input(INPUT_GET, 'id');
+    
+    deleteItemById($id);
+    
+    header ( "Location: index.php?uri=trash" );
+}
