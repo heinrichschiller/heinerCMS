cca21751622c8e725d193e4a6c7d3061f4a59902
heinrichschiller <heinrich@heinrich-schiller.de>
2018-11-01 13:41:24 +0100

    bugfix, wrong configuration

--- configs/db-config.php
+++ configs/db-config.php
@@ -2,8 +2,3 @@
 
 // content on this file will be creating by installer
 // for manual configuration, please read the README
-
-define('DB_DRIVER', 'sqlite');
-
-define('DB_NAME', __DIR__ . '/../data/sqlite/heinercms.db');
-define('PDO_DEBUG_MODE', true);
