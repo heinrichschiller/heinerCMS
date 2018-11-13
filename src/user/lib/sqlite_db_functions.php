a603a4894bf6273b3be88c0293ba792515fb0b57
heinrichschiller <heinrich@heinrich-schiller.de>
2018-11-13 11:24:35 +0100

    update, user functions

--- src/user/lib/sqlite_db_functions.php
+++ src/user/lib/sqlite_db_functions.php
@@ -29,8 +29,6 @@
 
 function loadUserStatement()
 {
-    $pdo = getPdoConnection();
-    
     $sql = '
     SELECT `id`,
         `firstname`,
@@ -40,6 +38,8 @@ function loadUserStatement()
         FROM `users`
             ORDER BY `firstname` DESC
     ';
+    
+    $pdo = getPdoConnection();
     
     try {
         $stmt = $pdo->prepare($sql);
@@ -62,8 +62,6 @@ function loadUserStatement()
  */
 function getUser(int $id) : array
 {
-    $pdo = getPdoConnection();
-    
     $sql = "
     SELECT `id`,
         `firstname`,
@@ -77,12 +75,127 @@ function getUser(int $id) : array
         WHERE `id` = :id
 	";
     
+    $pdo = getPdoConnection();
+    
     try {
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':id', $id);
         $stmt->execute();
         
         return $stmt->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $ex) {
+        echo $ex->getMessage();
+        exit();
+    }
+}
+
+/**
+ * Delete an user from 'users' table by id.
+ * 
+ * @param int $id - Id from an user entry.
+ * 
+ * @since 0.9.0
+ */
+function deleteUser(int $id)
+{
+    $sql = '
+    DELETE FROM `users` 
+        WHERE id = :id
+    ';
+    
+    $pdo = getPdoConnection();
+    
+    try {
+        $stmt = $pdo->prepare($sql);
+        
+        $stmt->bindParam(':id', $id);
+        
+        $stmt->execute();
+    } catch (PDOException $ex) {
+        echo $ex->getMessage();
+        exit();
+    }
+}
+
+function addUser()
+{
+    $sql ='INSERT INTO `users`(`
+        firstname`,
+        `lastname`,
+        `email`,
+        `password`,
+        `created_at`,
+        `username`,
+        `active`)
+        VALUES (:firstname,
+            :lastname,
+            :email,
+            :password,
+            :created_at,
+            :username,
+            :active)
+    ';
+    $pdo = getPdoConnection();
+    
+    try {
+        $stmt = $pdo->prepare($sql);
+        
+        $stmt->bindParam(':firstname', $firstname);
+        $stmt->bindParam(':lastname', $lastname);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $pwHash);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':active', $active);
+        
+        if (DB_DRIVER == 'sqlite') {
+            $stmt->bindParam(':created_at', $created_at);
+        }
+        
+        $stmt->execute();
+    } catch (PDOException $ex) {
+        echo $ex->getMessage();
+        exit();
+    }
+}
+
+/**
+ * 
+ * @param int $id
+ * @param string $firstname
+ * @param string $lastname
+ * @param string $username
+ * @param string $email
+ * @param string $active
+ */
+function updateUser(int $id,
+    string $firstname,
+    string $lastname,
+    string $username,
+    string $email,
+    string $active)
+{
+    $sql = '
+    UPDATE `users` SET `firstname`= :firstname,
+        `lastname`= :lastname,
+        `email`= :email,
+        `username`= :username,
+        `active`= :active,
+        WHERE `id` = :id
+    ';
+    
+    $pdo = getPdoConnection();
+    
+    try {
+        $stmt = $pdo->prepare($sql);
+        
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':firstname', $firstname);
+        $stmt->bindParam(':lastname', $lastname);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':active', $active);
+        
+        $stmt->execute();
     } catch (PDOException $ex) {
         echo $ex->getMessage();
         exit();
