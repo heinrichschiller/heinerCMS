314981f2cf5585d8a09cbb5c4500734311a148e7
heinrichschiller <heinrich@heinrich-schiller.de>
2018-11-19 18:14:21 +0100

    removed, because obsolete

--- src/templates/default/admin/js/svg-icon-webcomponent-master/entry.js
+++
@@ -1,8 +0,0 @@
-// this file means to be compiled to lib
-import {default as createFn} from "./icon";
-
-// change this if you want a different tag name 
-let tagName = "svg-icon";
-
-// run
-createFn(tagName)();
