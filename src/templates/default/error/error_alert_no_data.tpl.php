f7d1d2e6ba259798ca57ff35e6b45a4f46b167b7
Heinrich Schiller <heinrich@heinrich-schiller.de>
2018-11-30 06:50:57 +0100

    update, templates

--- src/templates/default/error/error_alert_no_data.tpl.php
+++
@@ -1,8 +0,0 @@
-<div class="alert alert-danger" role="alert">
-	<h4 class="alert-heading">Warnung!</h4>
-	<hr>
-	<p>Wahrscheinlich führen Sie heinerCMS zum erstem mal aus oder es ist ein Fehler aufgetretten.</p>
-	<p>Im Fehlerfall kontaktieren Sie bitte ihren Administrator!</p>
-	<p>Wenn Sie heinerCMS zum erstem mal verwerden, klicken Sie jetzt auf</p>
-	<a href="$PHP_SELF/../_installer_/index.php?uri=language" class="btn btn-primary" role="button">Installieren</a>
-</div>
