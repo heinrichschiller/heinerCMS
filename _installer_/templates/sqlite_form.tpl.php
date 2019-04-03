acfb69ae7288fb433d5b072f1f5b6aa7b5084c2a
Heinrich Schiller <heinrich@heinrich-schiller.de>
2019-04-03 20:56:52 +0200

    #CHANGE 'deleted, old installer because its obsolete'

--- _installer_/templates/sqlite_form.tpl.php
+++
@@ -1,7 +0,0 @@
-<form method="get" action="databaseinsert.php">
-    <p>Wenn sie SQLite ausgewählt haben, werden keine zugangdaten für eine Datenbank benötigt.</p>
-    <p>Sie können jetzt auf Weiter klicken.</p>
-    <button type="submit" class="btn btn-primary btn_bottom">{next}</button>
-    <input type="hidden" name="lang" value="##placeholder-lang##">
-    <input type="hidden" name="db" value="##placeholder-db##">
-</form>
