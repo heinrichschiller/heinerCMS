f2f2e917401632761bc6974deba9b85bf74f303f
Heinrich Schiller <heinrich@Heinrichs-MBP.Speedport_W723_V_Typ_A_1_01_018>
2019-05-01 14:56:18 +0200

    "#CHANGE 'new comments, renamed path, added translation'"

--- installer/src/lib/core.php
+++ installer/src/lib/core.php
@@ -27,10 +27,10 @@
  * SOFTWARE.
  */
 
-/**
- *
- * @since 0.12.0
- */
+ /**
+  * @source /src/lib/core.php
+  * @since 0.12.0
+  */
 function bootstrap()
 {
     $requestItems = parseRequest();
@@ -39,7 +39,7 @@ function bootstrap()
     $action     = $requestItems['action'];
     $params     = $requestItems['params'];
 
-    $ctrlPath = __DIR__ . "/../controller/$controller/index.php";
+    $ctrlPath = __DIR__ . "/../modules/$controller/index.php";
 
     if(file_exists($ctrlPath)) {
         include_once $ctrlPath;
@@ -60,7 +60,7 @@ function bootstrap()
 
 /**
  *
- *
+ * @source /src/lib/core.php
  * @since 0.12.0
  */
 function parseRequest(): array
@@ -87,6 +87,90 @@ function parseRequest(): array
     return $requestItems;
 }
 
+/**
+ * Get a XML-File for translations in heinerCMS.
+ *
+ * @param string $language - Name of language
+ *
+ * @return array
+ *
+ * @source /installer/src/lib/core.php
+ * @since 0.12.0
+ */
+function getTranslation(string $language) : array
+{
+    $xmlfile = __DIR__ . "/../locales/$language.xml";
+
+    $xmlString = file_get_contents($xmlfile);
+    $xml = simplexml_load_string($xmlString);
+
+    $arr_keys = [];
+    $arr_values = [];
+
+    foreach ($xml->children() as $second_gen) {
+        foreach ($second_gen->children() as $third_gen) {
+            array_push( $arr_keys, '{'.$third_gen->getName().'}');
+            array_push( $arr_values, (string)$third_gen);
+        }
+    }
+
+    return array_combine($arr_keys, $arr_values);
+}
+
+/**
+ * @source /src/lib/core.php
+ * @since 0.9.0
+ */
+function render(array $templates, array $data = [])
+{
+    $html = '';
+
+    //$settings = getGeneralSettings();
+    $arr_language = getTranslation('de');
+
+    foreach($templates as $key) {
+        $html .= renderTemplate($key, $data);
+    }
+
+    return strtr($html, $arr_language);
+}
+
+/**
+ * Render the template.
+ *
+ * @param string $tplName Name of a template.
+ * @param array  $data
+ *
+ * @source /src/lib/core.php
+ * @since 2019.02
+ */
+function renderTemplate(string $tplName, array $data)
+{
+    extract($data);
+
+    $requestItems = parseRequest();
+
+    ob_start();
+
+    $ctrl = !empty($requestItems['controller'])
+      ? $requestItems['controller']
+      : 'index';
+
+    $template = __DIR__ . "/../modules/$ctrl/templates/$tplName";
+
+    include __DIR__ . '/../template/main.phtml';
+
+    $htmlResponse = ob_get_contents();
+
+    ob_clean();
+
+    return $htmlResponse;
+}
+
+/**
+ * @source /src/lib/core.php
+ * @since 0.11.0
+ */
 function error404()
 {
     header('Content-Type:text/html;charset=utf-8');
