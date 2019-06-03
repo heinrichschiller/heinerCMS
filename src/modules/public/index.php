00e6fa44807f4b2197d5ae8dd34baa9cf94fb292
Heinrich Schiller <heinrich@heinrichs-mbp.speedport.ip>
2019-06-03 05:05:13 +0200

    "#CHANGE 'clean code. constants removed"

--- src/modules/public/index.php
+++ src/modules/public/index.php
@@ -27,30 +27,10 @@
  * SOFTWARE.
  */
 
-if(!defined('PUBLIC_PATH')) {
+$config = include ABS_PATH . 'src/configs/default.php';
 
-    /**
-     * Path to modules/public/
-     *
-     * @var string
-     *
-     * @since 0.9.0
-     */
-    define('PUBLIC_PATH', __DIR__ . '/');
-}
+$path = ABS_PATH . 'src/modules/public/libs/';
 
-if(!defined('PUBLIC_LIB_PATH')) {
-
-    /**
-     * Path to modules/public/lib/
-     *
-     * @var string
-     *
-     * @since 0.9.0
-     */
-    define('PUBLIC_LIB_PATH', __DIR__ . '/lib/');
-}
-
-include PUBLIC_LIB_PATH . DB_DRIVER .'-functions.php';
-include PUBLIC_LIB_PATH . 'actions.php';
-include PUBLIC_LIB_PATH . 'functions.php';
+require $path . $config['driver'] . '-functions.php';
+require $path . 'actions.php';
+require $path . 'functions.php';
