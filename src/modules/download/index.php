575bfe6dbc1a8e10560f30473456ccd3b762c2c5
Heinrich Schiller <heinrich@heinrich-schiller.de>
2019-07-27 23:27:00 +0200

    #CHANGE 'code update'

--- src/modules/download/index.php
+++ src/modules/download/index.php
@@ -27,28 +27,9 @@
  * SOFTWARE.
  */
 
-if(!defined('DOWNLOAD_PATH')) {
+ $path = ROOT_PATH . 'src/modules/download/src/';
 
-    /**
-     * Path to modules/download/.
-     *
-     * @var unknown
-     * @since 0.9.0
-     */
-    define('DOWNLOAD_PATH', __DIR__ . '/');
-}
+ include $path . 'actions.php';
+ include $path . 'functions.php';
 
-if(!defined('DOWNLOAD_LIB_PATH')) {
-
-    /**
-     * Path to modules/download/lib/.
-     *
-     * @var string
-     * @since 0.9.0
-     */
-    define('DOWNLOAD_LIB_PATH', __DIR__ . '/lib/');
-}
-
-include DOWNLOAD_LIB_PATH . DB_DRIVER . '-functions.php';
-include DOWNLOAD_LIB_PATH . 'actions.php';
-include DOWNLOAD_LIB_PATH . 'functions.php';
+ include $path . $configs['database']['driver'] . '-functions.php';
